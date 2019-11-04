b1e84e852de42f05a7f9d2253deaf86efa4c8013
League Student <league@iMac-9.attlocal.net>
2019-11-03 17:26:10 -0800

    Level1-Module4 || 03 November 2019

--- src/_06_book_of_illusions/BookOfIllusions.java
+++ src/_06_book_of_illusions/BookOfIllusions.java
@@ -11,31 +11,54 @@ import java.net.URL;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
  
 /** We’re going to make a slideshow of cool optical illusions. When the user clicks on an illusion, a new one will be loaded. **/
 
 public class BookOfIllusions extends MouseAdapter {
 
 	// 1. Make a JFrame variable and initialize it using "new JFrame()"
-
+	JFrame frame = new JFrame();
+	JLabel label1;
+	
 	public void run() {
 		// 2. make the frame visible
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
 		// 3. set the size of the frame
+		frame.setSize(672, 372);
+		JOptionPane.showMessageDialog(null, "What do you see first? A glass or faces?");
 		// 4. find 2 images and save them to your project’s default package
 		// 5. make a variable to hold the location of your image. e.g. "illusion.jpg"
+		String file1 = "illusion1.jpg";
 		// 6. create a variable of type "JLabel" but don’t initialize it yet
 		// 7. use the "loadImage..." methods below to initialize your JLabel
+		label1 = loadImageFromComputer(file1);
 		// 8. add your JLabel to the frame
+		frame.add(label1);
 		// 9. call the pack() method on the frame
+		frame.pack();
 		// 10. add a mouse listener to your frame (hint: use *this*)
+		frame.addMouseListener(this);
 	}
 
 	public void mousePressed(MouseEvent e) {
 		// 11. Print "clicked!" to the console when the mouse is pressed
+		if(e.equals(e)) {
+			System.out.println("Clicked!");
+		}
 		// 12. remove everything from the frame that was added earlier
+		frame.remove(label1);
+		JOptionPane.showMessageDialog(null, "Which line is longer?");
 		// 13. load a new image like before (this is more than one line of code)
+		String file2 = "illusion2.jpg";
+		JLabel label2;
+		label2 = loadImageFromComputer(file2);
+		frame.add(label2);
 		// 14. pack the frame
+		frame.pack();
 	}
 
 	// [OPTIONAL] 15. goad your users with some annoying or witty pop-ups
